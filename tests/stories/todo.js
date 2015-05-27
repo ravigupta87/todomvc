5b598e8ced4ce6400c89e2f1181c4a764d83cb51
ravigupta87 <ravi.gupta@accionlabs.com>
2015-05-27 15:21:43 +0530

    Pending test POC

---
+++ tests/stories/todo.js
@@ -0,0 +1,51 @@
+describe('todo page', function() {
+
+    it('should have the right page url');
+
+    it('should have the right browser title');
+
+    it('should have the right page title');
+
+    it('should have the placeholder in todo');
+
+    describe('todo items', function() {
+
+        it('should add item');
+
+        it('should have right item');
+
+        it('should edit item');
+
+        it('should complete item');
+
+        it('should delete item');
+ 
+        describe('todo item listing', function() {
+            
+            it('should have filters');
+
+            it('should have filter all');
+
+            it('should have filter active');
+
+            it('should have filter complete button');
+
+            it('should not have clear completed if no task completed');
+
+            it('should clear completed once task completed');
+
+            it('should have remaining 1');
+
+            it('should filter all');
+
+            it('should filter active');
+
+            it('should filter completed');
+
+            it('should clear completed');
+
+        });
+    });
+
+});
+
